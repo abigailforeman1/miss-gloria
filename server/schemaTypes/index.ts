b060cc214bec402f9b39e7c3887aeae317820daf
Abigail <abigail@picnic-media.com>
2025-05-28 17:40:39 +0100

    Initial commit with Sanity and Next.js

---
+++ server/schemaTypes/index.ts
@@ -0,0 +1,6 @@
+import blockContent from './blockContent'
+import service from './service'
+import project from './project'
+// import author from './author'
+
+export const schemaTypes = [project, service, blockContent]
---
+++ server/schemaTypes/project.ts
@@ -0,0 +1,76 @@
+import {defineField, defineType} from 'sanity'
+
+export default defineType({
+  name: 'project',
+  title: 'Project',
+  type: 'document',
+  fields: [
+    defineField({
+      name: 'client',
+      title: 'Client',
+      type: 'string',
+    }),
+    defineField({
+      name: 'title',
+      title: 'Title',
+      type: 'string',
+    }),
+    defineField({
+      name: 'slug',
+      title: 'Slug',
+      type: 'slug',
+      options: {
+        source: 'title',
+        maxLength: 96,
+      },
+    }),
+    defineField({
+      name: 'mainImage',
+      title: 'Main image',
+      type: 'image',
+      options: {
+        hotspot: true,
+      },
+    }),
+    defineField({
+      name: 'year',
+      title: 'Year',
+      type: 'number',
+    }),
+    // defineField({
+    //   name: 'author',
+    //   title: 'Author',
+    //   type: 'reference',
+    //   to: {type: 'author'},
+    // }),
+    defineField({
+      name: 'services',
+      title: 'Services',
+      type: 'array',
+      of: [{type: 'reference', to: {type: 'service'}}],
+    }),
+    defineField({
+      name: 'publishedAt',
+      title: 'Published at',
+      type: 'datetime',
+    }),
+    defineField({
+      name: 'body',
+      title: 'Body',
+      type: 'blockContent',
+    }),
+  ],
+
+  preview: {
+    select: {
+      client: 'client',
+      title: 'title',
+      // author: 'author.name',
+      media: 'mainImage',
+    },
+    prepare(selection) {
+      const {client} = selection
+      return {...selection, subtitle: `weird test ${client}`}
+    },
+  },
+})
