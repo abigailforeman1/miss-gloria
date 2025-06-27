e3494ace8ca0e5a9693fa1a71b57c28ed48d20ed
Abigail <abigail@picnic-media.com>
2025-06-27 17:50:33 +0100

    scheme updates

--- server/schemaTypes/index.ts
+++ server/schemaTypes/index.ts
@@ -1,5 +1,4 @@
-import blockContent from './blockContent'
 import service from './service'
 import project from './project'
 
-export const schemaTypes = [project, service, blockContent]
+export const schemaTypes = [project, service]
--- server/schemaTypes/project.ts
+++ server/schemaTypes/project.ts
@@ -97,7 +97,7 @@ export default defineType({
     defineField({
       name: 'body',
       title: 'Body',
-      type: 'blockContent',
+      type: 'string',
     }),
   ],
 
